31d1718c8f4ad0d238455870583f3b4bba4bfc55
Miffy <miffygithub@gmail.com>
2023-01-24 09:55:20 -0800

    feat: Add HW5: Drop down

---
+++ HW5/index.js
@@ -0,0 +1,44 @@
+
+const state = {
+  'a': 'apple',
+  'b': 'banana',
+  'c': 'city',
+  'd': 'dom',
+  'e': 'end',
+  'f': 'forEach',
+  'g': 'gold',
+};
+
+function makeDrop(){
+    let dropDown = document.createElement('div');
+    let selectKey = document.createElement('select');
+    let selectValue = document.createElement('select');
+    // let test = document.createElement('select');
+    let _state = {}
+    function createDorpDown(){
+        for(let i in state){
+            _state[state[i]] = i;
+            let keyOp = document.createElement('option');
+            let valueOp = document.createElement('option');
+            // console.log(i);
+            keyOp.value = keyOp.innerHTML = i;
+            valueOp.value = valueOp.innerHTML = state[i];
+            selectKey.append(keyOp);
+            selectValue.append(valueOp);
+        }
+        dropDown.appendChild(selectKey);
+        dropDown.appendChild(selectValue);
+        // dropDown.appendChild(test);
+    }
+    createDorpDown();
+    document.body.appendChild(dropDown);
+    selectKey.onchange = (event)=>{
+        let newValue = state[event.target.value];
+        selectValue.value = newValue;
+    }
+    selectValue.onchange = (event)=>{
+        let newValue = _state[event.target.value];
+        selectKey.value = newValue;
+    }
+}
+makeDrop();
