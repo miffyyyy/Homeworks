75eafc77c9848eeb33f261178f24e4ab9431e094
Miffy <miffygithub@gmail.com>
2023-02-23 02:43:27 -0800

    Setup structure

---
+++ book-search-app/src/app/wish-item/wish-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-item',
+  templateUrl: './wish-item.component.html',
+  styleUrls: ['./wish-item.component.scss']
+})
+export class WishItemComponent {
+
+}
