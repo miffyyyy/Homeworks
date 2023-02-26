b1404b265322cdd463604bacc7cb4abd900ed5b5
Miffy <miffygithub@gmail.com>
2023-02-26 15:41:38 -0800

    feature: enable show wishlist component

--- book-search-app/src/app/home-page/home-page.component.ts
+++ book-search-app/src/app/home-page/home-page.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-
+  showWishlist: boolean = false;
+  constructor() {
+    this.showWishlistHandler = this.showWishlistHandler.bind(this);
+    this.showHomeHandler = this.showHomeHandler.bind(this);
+  }
+  showWishlistHandler() {
+    this.showWishlist = true;
+  }
+  showHomeHandler() {
+    this.showWishlist = false;
+  }
 }
